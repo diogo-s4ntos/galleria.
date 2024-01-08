4a201fbcac9416cb43443e735c54a6bcf352ba35
Diogo Santos <117995697+diogo-s4ntos@users.noreply.github.com>
2024-01-08 14:18:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function info(element){
+    var imageInfo = element.nextElementSibling;
+
+    imageInfo.style.display = 'flex';
+}
+
+function noneInfo(element){
+    var imageInfo = element.nextElementSibling;
+
+    imageInfo.style.display = 'none';
+}
